4552bc93a83e0d502ac048f83bc369fdf07a41c5
Jacob Oakman <jacoboakman@gmail.com>
2020-12-22 15:35:09 -0500

    Uncommented code, fixed spaces to tabs

--- src/main.rs
+++ src/main.rs
@@ -15,100 +15,100 @@ use std::time::Instant;
 type AccountId = usize;
 
 enum Command {
-    Transfer {
-        from: AccountId,
-        to: AccountId,
-        amount: f64,
+	Transfer {
+		from: AccountId,
+		to: AccountId,
+		amount: f64,
 		},
 		CheckTotalBalance,
 }
 
 fn main() {
-    // Configure parameters
-    let stripe_count = 256;
-    let account_count = 1024;
-    let command_count = 100000;
-    let thread_count = 1;
+	// Configure parameters
+	let stripe_count = 256;
+	let account_count = 1024;
+	let command_count = 100000;
+	let thread_count = 1;
 
-    let accounts = Arc::new(Accounts::new(stripe_count));
+	let accounts = Arc::new(Accounts::new(stripe_count));
 
-    for id in 0..account_count {
-        accounts.add_account(id, 100000.0 / account_count as f64)
-    }
+	for id in 0..account_count {
+		accounts.add_account(id, 100000.0 / account_count as f64)
+	}
 
-    // Generate random commands.
-    let mut rng = Pcg64::seed_from_u64(0);
-    let command_range = Uniform::from(0..20);
+	// Generate random commands.
+	let mut rng = Pcg64::seed_from_u64(0);
+	let command_range = Uniform::from(0..20);
 		let account_range = Uniform::from(0..account_count);
 		
 		// Calculate range as integer so it can be converted to 2 decimal places
-    let deposit_range = Uniform::from(0..10000);
+	let deposit_range = Uniform::from(0..10000);
 
-    let commands: Arc<[Command]> = (0..command_count)
-        .map(|_| {
-            if command_range.sample(&mut rng) == 0 {
-                Command::CheckTotalBalance
-            } else {
-                Command::Transfer {
-                    from: account_range.sample(&mut rng),
-                    to: account_range.sample(&mut rng),
-                    amount: deposit_range.sample(&mut rng) as f64 / 100.0,
-                }
-            }
-        })
-        .collect();
+	let commands: Arc<[Command]> = (0..command_count)
+		.map(|_| {
+			if command_range.sample(&mut rng) == 0 {
+				Command::CheckTotalBalance
+			} else {
+				Command::Transfer {
+					from: account_range.sample(&mut rng),
+					to: account_range.sample(&mut rng),
+					amount: deposit_range.sample(&mut rng) as f64 / 100.0,
+				}
+			}
+		})
+		.collect();
 
-    let start = Instant::now();
-    let mut threads = vec![];
+	let start = Instant::now();
+	let mut threads = vec![];
 
-    let mut begin = 0;
-    let mut end = command_count / thread_count;
-    let mut spare = command_count % thread_count;
+	let mut begin = 0;
+	let mut end = command_count / thread_count;
+	let mut spare = command_count % thread_count;
 
-    for _ in 0..thread_count {
-        // Distribute remainder evenly
-        if spare > 0 {
-            end += 1;
-            spare -= 1;
-        }
+	for _ in 0..thread_count {
+		// Distribute remainder evenly
+		if spare > 0 {
+			end += 1;
+			spare -= 1;
+		}
 
-        let accounts = Arc::clone(&accounts);
-        let commands = Arc::clone(&commands);
+		let accounts = Arc::clone(&accounts);
+		let commands = Arc::clone(&commands);
 
-        threads.push(thread::spawn(move || {
+		threads.push(thread::spawn(move || {
 					do_work(&accounts, &commands[begin..end])
-        }));
+		}));
 
-        // Shift slice window
-        begin = end;
-        end += command_count / thread_count;
-    }
+		// Shift slice window
+		begin = end;
+		end += command_count / thread_count;
+	}
 
-    let max_time = threads
-        .into_iter()
-        .map(|handle| handle.join().unwrap())
-        .max()
-        .unwrap();
+	let max_time = threads
+		.into_iter()
+		.map(|handle| handle.join().unwrap())
+		.max()
+		.unwrap();
 
-    println!("Max time {}", max_time);
-    println!("Total {}", start.elapsed().as_micros());
+	println!("Max time {}", max_time);
+	println!("Total {}", start.elapsed().as_micros());
 }
 
 fn do_work(accounts: &Accounts, commands: &[Command]) -> u128 {
-    let start = std::time::Instant::now();
+	let start = std::time::Instant::now();
 
-    for command in commands {
-        match command {
-            &Command::Transfer { from, to, amount } => {
-                // println!("starting transfer");
-                accounts.transfer(from, to, amount).expect("account doesn't exist");
-                // println!("ending transfer");
-            },
-            Command::CheckTotalBalance => {
-                println!("Total Balance {}", accounts.sum_up_all_accounts());
-            }
-        }
-    }
+	for command in commands {
+		match command {
+			&Command::Transfer { from, to, amount } => {
+				// println!("starting transfer");
+				accounts.transfer(from, to, amount).expect("account doesn't exist");
+				// println!("ending transfer");
+			},
+			Command::CheckTotalBalance => {
+				println!("Total Balance {}", accounts.sum_up_all_accounts());
+			}
+		}
+	}
 
-    start.elapsed().as_micros()
+	start.elapsed().as_micros()
 }
