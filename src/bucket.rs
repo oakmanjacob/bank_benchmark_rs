ebb626a74fa9c68d9eadc1facf7f4e349ae50acd
Lachlan Sneff <lachlan.sneff@gmail.com>
2020-12-22 14:05:49 -0500

    Change tabs to spaces

--- src/bucket.rs
+++ src/bucket.rs
@@ -3,127 +3,127 @@ use std::sync::Mutex;
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 pub enum AccountError {
-	DoesNotExist,
+    DoesNotExist,
 }
 
 pub struct Accounts {
-	buckets: Box<[Mutex<Bucket<usize, f64>>]>,
+    buckets: Box<[Mutex<Bucket<usize, f64>>]>,
 }
 
 impl Accounts {
-	pub fn new(buckets: usize) -> Self {
-		let buckets: Vec<_> = (0..buckets).map(|_| Mutex::new(Bucket::new())).collect();
-		
-		Self {
-			buckets: buckets.into_boxed_slice(),
-		}
-	}
+    pub fn new(buckets: usize) -> Self {
+        let buckets: Vec<_> = (0..buckets).map(|_| Mutex::new(Bucket::new())).collect();
+        
+        Self {
+            buckets: buckets.into_boxed_slice(),
+        }
+    }
 
-	pub fn transfer(&self, from: AccountId, to: AccountId, amount: f64) -> Result<(), AccountError> {
-		let from_bucket_idx = from % self.buckets.len();
-		let to_bucket_idx = to % self.buckets.len();
+    pub fn transfer(&self, from: AccountId, to: AccountId, amount: f64) -> Result<(), AccountError> {
+        let from_bucket_idx = from % self.buckets.len();
+        let to_bucket_idx = to % self.buckets.len();
 
-		if from_bucket_idx == to_bucket_idx {
-			// Same bucket.
-			let mut bucket = self.buckets[from_bucket_idx].lock().unwrap();
+        if from_bucket_idx == to_bucket_idx {
+            // Same bucket.
+            let mut bucket = self.buckets[from_bucket_idx].lock().unwrap();
 
-			let from_amount = bucket.get(from).ok_or(AccountError::DoesNotExist)?.1;
-			let to_amount = bucket.get(to).ok_or(AccountError::DoesNotExist)?.1;
-			bucket.update(from, from_amount - amount);
-			bucket.update(to, to_amount + amount);
-		} else {
-			// Different buckets.
-			// We have to be careful here, we must always lock in the same order to prevent deadlocks.
-			let (mut from_bucket, mut to_bucket) = if from_bucket_idx < to_bucket_idx {
-				let from_bucket = self.buckets[from_bucket_idx].lock().unwrap();
-				let to_bucket = self.buckets[to_bucket_idx].lock().unwrap();
-				(from_bucket, to_bucket)
-			} else {
-				let to_bucket = self.buckets[to_bucket_idx].lock().unwrap();
-				let from_bucket = self.buckets[from_bucket_idx].lock().unwrap();
-				(from_bucket, to_bucket)
-			};
+            let from_amount = bucket.get(from).ok_or(AccountError::DoesNotExist)?.1;
+            let to_amount = bucket.get(to).ok_or(AccountError::DoesNotExist)?.1;
+            bucket.update(from, from_amount - amount);
+            bucket.update(to, to_amount + amount);
+        } else {
+            // Different buckets.
+            // We have to be careful here, we must always lock in the same order to prevent deadlocks.
+            let (mut from_bucket, mut to_bucket) = if from_bucket_idx < to_bucket_idx {
+                let from_bucket = self.buckets[from_bucket_idx].lock().unwrap();
+                let to_bucket = self.buckets[to_bucket_idx].lock().unwrap();
+                (from_bucket, to_bucket)
+            } else {
+                let to_bucket = self.buckets[to_bucket_idx].lock().unwrap();
+                let from_bucket = self.buckets[from_bucket_idx].lock().unwrap();
+                (from_bucket, to_bucket)
+            };
 
-			let from_amount = from_bucket.get(from).ok_or(AccountError::DoesNotExist)?.1;
-			let to_amount = to_bucket.get(to).ok_or(AccountError::DoesNotExist)?.1;
-			from_bucket.update(from, from_amount - amount);
-			to_bucket.update(to, to_amount + amount);
-		}
+            let from_amount = from_bucket.get(from).ok_or(AccountError::DoesNotExist)?.1;
+            let to_amount = to_bucket.get(to).ok_or(AccountError::DoesNotExist)?.1;
+            from_bucket.update(from, from_amount - amount);
+            to_bucket.update(to, to_amount + amount);
+        }
 
-		Ok(())
-	}
+        Ok(())
+    }
 
-	pub fn sum_up_all_accounts(&self) -> f64 {
-		let guards: Vec<_> = self.buckets.iter().map(|bucket| bucket.lock().unwrap()).collect();
+    pub fn sum_up_all_accounts(&self) -> f64 {
+        let guards: Vec<_> = self.buckets.iter().map(|bucket| bucket.lock().unwrap()).collect();
 
-		guards
-			.into_iter()
-			.map(|bucket| -> f64 {
-				bucket.map.iter().map(|(_, balance)| balance).sum()
-			})
-			.sum()
+        guards
+            .into_iter()
+            .map(|bucket| -> f64 {
+                bucket.map.iter().map(|(_, balance)| balance).sum()
+            })
+            .sum()
 
-	}
+    }
 
-	pub fn add_account(&self, id: AccountId, balance: f64) {
-		let mut bucket = self.buckets[id % self.buckets.len()].lock().unwrap();
-		assert!(bucket.insert(id, balance), "account already exists");
-	}
+    pub fn add_account(&self, id: AccountId, balance: f64) {
+        let mut bucket = self.buckets[id % self.buckets.len()].lock().unwrap();
+        assert!(bucket.insert(id, balance), "account already exists");
+    }
 }
 
 pub struct Bucket<K, V> {
-	map: Vec<(K, V)>
+    map: Vec<(K, V)>
 }
 
 impl<K: Ord + Copy, V: Copy> Bucket<K, V> {
-	pub fn new() -> Self {
-		Self {
-			map: Vec::new()
-		}
-	}
+    pub fn new() -> Self {
+        Self {
+            map: Vec::new()
+        }
+    }
 
-	pub fn insert(&mut self, key: K, value: V) -> bool {
-		match self.map.binary_search_by_key(&key, |(k,_)| *k) {
-			Ok(_) => false,
-			Err(index) => {
-				self.map.insert(index, (key, value));
-				true
-			},
-		}
-	}
+    pub fn insert(&mut self, key: K, value: V) -> bool {
+        match self.map.binary_search_by_key(&key, |(k,_)| *k) {
+            Ok(_) => false,
+            Err(index) => {
+                self.map.insert(index, (key, value));
+                true
+            },
+        }
+    }
 
-	pub fn update(&mut self, key: K, value: V) -> bool {
-		match self.map.binary_search_by_key(&key, |(k,_)| *k) {
-			Ok(index) => {
-				self.map[index].1 = value;
-				true
-			},
-			Err(_) => {
-				false
-			},
-		}
-	}
+    pub fn update(&mut self, key: K, value: V) -> bool {
+        match self.map.binary_search_by_key(&key, |(k,_)| *k) {
+            Ok(index) => {
+                self.map[index].1 = value;
+                true
+            },
+            Err(_) => {
+                false
+            },
+        }
+    }
 
-	// pub fn remove(&mut self, key: K) -> bool {
-	// 	match self.map.binary_search_by_key(&key, |(k,_)| *k) {
-	// 		Ok(index) => {
-	// 			self.map.remove(index);
-	// 			true
-	// 		},
-	// 		Err(_) => {
-	// 			false
-	// 		},
-	// 	}
-	// }
+    // pub fn remove(&mut self, key: K) -> bool {
+    // 	match self.map.binary_search_by_key(&key, |(k,_)| *k) {
+    // 		Ok(index) => {
+    // 			self.map.remove(index);
+    // 			true
+    // 		},
+    // 		Err(_) => {
+    // 			false
+    // 		},
+    // 	}
+    // }
 
-	pub fn get(&self, key: K) -> Option<&(K, V)> {
-		match self.map.binary_search_by_key(&key, |(k,_)| *k) {
-			Ok(index) => {
-				Some(&self.map[index])
-			},
-			Err(_) => {
-				None
-			},
-		}
-	}
+    pub fn get(&self, key: K) -> Option<&(K, V)> {
+        match self.map.binary_search_by_key(&key, |(k,_)| *k) {
+            Ok(index) => {
+                Some(&self.map[index])
+            },
+            Err(_) => {
+                None
+            },
+        }
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -11,98 +11,98 @@ use std::time::Instant;
 type AccountId = usize;
 
 enum Command {
-	Transfer {
-		from: AccountId,
-		to: AccountId,
-		amount: f64,
-	},
-	CheckTotalBalance,
+    Transfer {
+        from: AccountId,
+        to: AccountId,
+        amount: f64,
+    },
+    CheckTotalBalance,
 }
 
 fn main() {
-	// Configure parameters
-	let stripe_count = 256;
-	let account_count = 1024;
-	let command_count = 1024;
-	let thread_count = 4;
+    // Configure parameters
+    let stripe_count = 256;
+    let account_count = 1024;
+    let command_count = 1024;
+    let thread_count = 4;
 
-	let accounts = Arc::new(Accounts::new(stripe_count));
+    let accounts = Arc::new(Accounts::new(stripe_count));
 
-	for id in 0..account_count {
-		accounts.add_account(id, 100000.0 / account_count as f64)
-	}
+    for id in 0..account_count {
+        accounts.add_account(id, 100000.0 / account_count as f64)
+    }
 
-	// Generate random commands.
-	let mut rng = rand::thread_rng();
-	let command_range = Uniform::from(0..20);
-	let account_range = Uniform::from(0..account_count);
-	let deposit_range = Uniform::from(0.0..100.0);
+    // Generate random commands.
+    let mut rng = rand::thread_rng();
+    let command_range = Uniform::from(0..20);
+    let account_range = Uniform::from(0..account_count);
+    let deposit_range = Uniform::from(0.0..100.0);
 
-	let commands: Arc<[Command]> = (0..command_count)
-		.map(|_| {
-			if command_range.sample(&mut rng) == 0 {
-				Command::CheckTotalBalance
-			} else {
-				Command::Transfer {
-					from: account_range.sample(&mut rng),
-					to: account_range.sample(&mut rng),
-					amount: deposit_range.sample(&mut rng),
-				}
-			}
-		})
-		.collect();
+    let commands: Arc<[Command]> = (0..command_count)
+        .map(|_| {
+            if command_range.sample(&mut rng) == 0 {
+                Command::CheckTotalBalance
+            } else {
+                Command::Transfer {
+                    from: account_range.sample(&mut rng),
+                    to: account_range.sample(&mut rng),
+                    amount: deposit_range.sample(&mut rng),
+                }
+            }
+        })
+        .collect();
 
-	let start = Instant::now();
-	let mut threads = vec![];
+    let start = Instant::now();
+    let mut threads = vec![];
 
-	let mut begin = 0;
-	let mut end = command_count / thread_count;
-	let mut spare = command_count % thread_count;
+    let mut begin = 0;
+    let mut end = command_count / thread_count;
+    let mut spare = command_count % thread_count;
 
-	for _ in 0..thread_count {
-		// Distribute remainder evenly
-		if spare > 0 {
-			end += 1;
-			spare -= 1;
-		}
+    for _ in 0..thread_count {
+        // Distribute remainder evenly
+        if spare > 0 {
+            end += 1;
+            spare -= 1;
+        }
 
-		let accounts = Arc::clone(&accounts);
-		let commands = Arc::clone(&commands);
+        let accounts = Arc::clone(&accounts);
+        let commands = Arc::clone(&commands);
 
-		threads.push(thread::spawn(move || {
-			do_work2(&accounts, &commands[begin..end])
-		}));
+        threads.push(thread::spawn(move || {
+            do_work2(&accounts, &commands[begin..end])
+        }));
 
-		// Shift slice window
-		begin = end;
-		end += command_count / thread_count;
-	}
+        // Shift slice window
+        begin = end;
+        end += command_count / thread_count;
+    }
 
-	let max_time = threads
-		.into_iter()
-		.map(|handle| handle.join().unwrap())
-		.max()
-		.unwrap();
+    let max_time = threads
+        .into_iter()
+        .map(|handle| handle.join().unwrap())
+        .max()
+        .unwrap();
 
-	println!("Max time {}", max_time);
-	println!("Total {}", start.elapsed().as_micros());
+    println!("Max time {}", max_time);
+    println!("Total {}", start.elapsed().as_micros());
 }
 
 fn do_work2(accounts: &Accounts, commands: &[Command]) -> u128 {
-	let start = std::time::Instant::now();
+    let start = std::time::Instant::now();
 
-	for command in commands {
-		match command {
-			&Command::Transfer { from, to, amount } => {
-				println!("starting transfer");
-				accounts.transfer(from, to, amount).expect("account doesn't exist");
-				println!("ending transfer");
-			},
-			Command::CheckTotalBalance => {
-				println!("Total Balance {}", accounts.sum_up_all_accounts());
-			}
-		}
-	}
+    for command in commands {
+        match command {
+            &Command::Transfer { from, to, amount } => {
+                println!("starting transfer");
+                accounts.transfer(from, to, amount).expect("account doesn't exist");
+                println!("ending transfer");
+            },
+            Command::CheckTotalBalance => {
+                println!("Total Balance {}", accounts.sum_up_all_accounts());
+            }
+        }
+    }
 
-	start.elapsed().as_micros()
+    start.elapsed().as_micros()
 }
