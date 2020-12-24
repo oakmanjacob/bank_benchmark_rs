1e288d13f300094c92baace0f1ef841362d44ac5
Jacob Oakman <jacoboakman@gmail.com>
2020-12-23 21:37:22 -0500

    Changed configuration to be CLI arguments

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,3 @@
-extern crate rand;
-extern crate rand_pcg;
-
 mod bucket;
 
 use rand::distributions::{Distribution, Uniform};
@@ -14,6 +11,8 @@ use std::time::Instant;
 use bucket::Accounts;
 use bucket::AccountsSeq;
 
+use structopt::StructOpt;
+
 type AccountId = usize;
 
 enum Command {
@@ -25,12 +24,23 @@ enum Command {
 	CheckTotalBalance,
 }
 
+#[derive(StructOpt)]
+struct Cli {
+	#[structopt(short = "a", long = "account_count", default_value = "1024")]
+	account_count: usize,
+
+	#[structopt(short = "s", long = "stripe_count", default_value = "256")]
+	stripe_count: usize,
+
+	#[structopt(short = "c", long = "command_count", default_value = "1024")]
+	command_count: usize,
+
+	#[structopt(short = "t", long = "thread_count", default_value = "1")]
+	thread_count: usize,
+}
+
 fn main() {
-	// Configure parameters
-	let stripe_count = 256;
-	let account_count = 1024;
-	let command_count = 1048576;
-	let thread_count = 1;
+	let args = Cli::from_args();
 
 	// Generate random commands.
 	// Commands will have a 5% change to be a balance command
@@ -38,10 +48,10 @@ fn main() {
 
 	let mut rng = Pcg64::seed_from_u64(0);
 	let command_range = Uniform::from(0..20);
-	let account_range = Uniform::from(0..account_count);
+	let account_range = Uniform::from(0..args.account_count);
 	let deposit_range = Uniform::from(0.0..100.0);
 
-	let commands: Arc<[Command]> = (0..command_count)
+	let commands: Arc<[Command]> = (0..args.command_count)
 		.map(|_| {
 			if command_range.sample(&mut rng) == 0 {
 				Command::CheckTotalBalance
@@ -57,20 +67,20 @@ fn main() {
 
 	let exec_time;
 
-	if thread_count > 1 {
+	if args.thread_count > 1 {
 		// Initialize accounts
-		let accounts = Arc::new(Accounts::new(stripe_count));
-		for id in 0..account_count {
-			accounts.add_account(id, 100000.0 / account_count as f64);
+		let accounts = Arc::new(Accounts::new(args.stripe_count));
+		for id in 0..args.account_count {
+			accounts.add_account(id, 100000.0 / args.account_count as f64);
 		}
 
 		let mut threads = vec![];
 
 		let mut begin = 0;
-		let mut end = command_count / thread_count;
-		let mut spare = command_count % thread_count;
+		let mut end = args.command_count / args.thread_count;
+		let mut spare = args.command_count % args.thread_count;
 
-		for _ in 0..thread_count {
+		for _ in 0..args.thread_count {
 			// Distribute remainder evenly
 			if spare > 0 {
 				end += 1;
@@ -86,7 +96,7 @@ fn main() {
 
 			// Shift slice window
 			begin = end;
-			end += command_count / thread_count;
+			end += args.command_count / args.thread_count;
 		}
 
 		// Reclaim execution
@@ -100,9 +110,9 @@ fn main() {
 	}
 	else {
 		// Initialize accounts sequential
-		let mut account_seq = AccountsSeq::new(stripe_count);
-		for id in 0..account_count {
-			account_seq.add_account(id, 100000.0 / account_count as f64);
+		let mut account_seq = AccountsSeq::new(args.stripe_count);
+		for id in 0..args.account_count {
+			account_seq.add_account(id, 100000.0 / args.account_count as f64);
 		}
 		
 		let start = Instant::now();
@@ -113,7 +123,7 @@ fn main() {
 					account_seq.transfer(from, to, amount).expect("account doesn't exist");
 				},
 				Command::CheckTotalBalance => {
-					println!("Total Balance {}", account_seq.sum_up_all_accounts());
+					println!("Total balance: {}", account_seq.sum_up_all_accounts());
 				}
 			}
 		}
@@ -137,7 +147,7 @@ fn do_work(accounts: &Accounts, commands: &[Command]) -> u128 {
 				accounts.transfer(from, to, amount).expect("account doesn't exist");
 			},
 			Command::CheckTotalBalance => {
-				println!("Total Balance {}", accounts.sum_up_all_accounts());
+				println!("Total balance: {}", accounts.sum_up_all_accounts());
 			}
 		}
 	}
